bd494d80deef30b5f275a28e53d4be4bda006b83
Paul Diones <dionespaul@gmail.com>
2017-11-18 20:11:52 -0800

    fixed htmlRoutes.html ->survey.html

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -12,7 +12,7 @@ module.exports = function(app) {
   // In each of the below cases the user is shown an HTML page of content
   // ---------------------------------------------------------------------------
   app.get("/survey", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/tables.html"));
+    res.sendFile(path.join(__dirname, "../public/survey.html"));
   });
   // If no matching route is found default to home
   app.get("*", function(req, res) {
